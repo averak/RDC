66a9df9016f8cc00b96bfb3ce67629725ef7d036
AjxLab <abe12@mccc.jp>
2020-02-23 16:48:23 +0900

    scoreの範囲を（0.0~100.0に設定）

--- joke_engine/joke/models.py
+++ joke_engine/joke/models.py
@@ -1,6 +1,6 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
 
 class Joke(models.Model):
     ## -----*----- Joke Model -----*----- ##
@@ -10,10 +10,21 @@ class Joke(models.Model):
     # score ：スコア（0.0~100.0）
     # date  ：作成日（変更不可）
 
-    joke = models.CharField(max_length=100)
-    author = models.CharField(max_length=100, null=True)
-    score = models.FloatField(default=0)
-    date = models.DateTimeField(auto_now_add=True)
+    joke = models.CharField(
+        max_length=100
+    )
+    author = models.CharField(
+        max_length=100,
+        null=True
+    )
+    score = models.FloatField(
+        default=0,
+        validators=[MinValueValidator(0.0),
+                    MaxValueValidator(100.0)]
+    )
+    date = models.DateTimeField(
+        auto_now_add=True
+    )
 
     def __str__(self):
         return self.joke
