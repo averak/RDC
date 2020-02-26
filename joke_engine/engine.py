f28f5df2457cf2a2306dc02cbbcd923a446c9512
AjxLab <abe12@mccc.jp>
2020-02-27 06:04:06 +0900

    記号を削除

--- joke_engine/engine.py
+++ joke_engine/engine.py
@@ -1,13 +1,31 @@
 # -*- coding: utf-8 -*-
+import re
 from janome.tokenizer import Tokenizer
+
 
 t = Tokenizer()
 
+
 def to_katakana(sentence):
     ## -----*----- カタカナ変換 -----*----- ##
-    katakana = [token.reading for token in t.tokenize(sentence)]
+    katakana = ''
+    for token in t.tokenize(sentence):
+        s = token.reading
+        if re.match('^[ァ-ンヴー]*$', s) != None:
+            katakana += s
 
-    return ''.join(katakana)
+    return katakana
+
+
+def is_joke(sentence):
+    ## -----*----- ダジャレ判定 -----*----- ##
+    # カタカナに変換
+    katakana = to_katakana(sentence)
+    # カタカナの長さ
+    l_katakana = len(katakana)
+    print(re.match('^[ァ-ンヴー]*$', katakana).group())
+    print(l_katakana)
 
 if __name__ == '__main__':
     print(to_katakana('遠距離恋愛'))
+    is_joke('遠距離恋愛')
