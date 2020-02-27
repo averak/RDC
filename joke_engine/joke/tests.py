32df911d8fa957dd2e1d8f438ec96332f57cf81c
AjxLab <abe12@mccc.jp>
2020-02-27 18:51:18 +0900

    add ダジャレ判定 tes

--- joke_engine/joke/tests.py
+++ joke_engine/joke/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+import requests
+import json
+
+
+url = "http://localhost:8000/joke"
+
+headers = {'Content-type': 'application/json'}
+joke = '布団が吹っ飛んだ'
+
+
+# ダジャレ判定API
+res = requests.get(url + '/judge?joke=%s' % joke)
+res_json = json.loads(res.text)
+print('''
+ダジャレ判定
+    - ダジャレ：{}
+    - レスポンス：{}
+'''.format(joke, res_json))
