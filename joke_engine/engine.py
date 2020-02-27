fcf364009fcf90f072ac6a422a0141bce80f1796
AjxLab <abe12@mccc.jp>
2020-02-28 05:40:44 +0900

    精度を向上

--- joke_engine/engine.py
+++ joke_engine/engine.py
@@ -9,11 +9,17 @@ t = Tokenizer()
 def to_katakana(sentence):
     ## -----*----- カタカナ変換 -----*----- ##
     katakana = ''
+
+    # 形態素解析
     for token in t.tokenize(sentence):
         s = token.reading
-        if s == '*' and re.match('[\u3041-\u309Fー]+', token.base_form) != None:
-            s = token.base_form
-        if re.match('^[ぁ-んァ-ンヴー]*$', s) != None:
+
+        if s == '*':
+            # 読みがわからないトークン
+            if re.match('[ぁ-んァ-ンー]+', token.surface) != None:
+                katakana += token.surface
+        else:
+            # 読みがわかるトークン
             katakana += s
 
     katakana = katakana.replace('ッ', '')
@@ -31,12 +37,10 @@ def is_joke(sentence, n=3):
     ## -----*----- ダジャレ判定 -----*----- ##
     # カタカナに変換
     katakana = to_katakana(sentence)
-    # カタカナの長さ
-    l_katakana = len(katakana)
 
     # 1文字ずつずらしてn文字の要素を作成
     col = []
-    for i in range(l_katakana-n+1):
+    for i in range(len(katakana)-n+1):
         col.append(katakana[i:(i+n)])
 
     if len(set(col)) != len(col):
@@ -47,6 +51,7 @@ def is_joke(sentence, n=3):
 
 if __name__ == '__main__':
     print(is_joke('遠距離恋愛'))
+    print(is_joke('布団が吹っ飛んだ'))
     print(is_joke('つくねがくっつくね'))
     print(is_joke('ソースを読んで納得したプログラマ「そーすね」'))
     print(is_joke('布団が吹っ飛んだ'))
