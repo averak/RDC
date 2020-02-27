9e089ca39ecde144dd3fc2f92ce99ad2f84d8a77
AjxLab <abe12@mccc.jp>
2020-02-27 18:56:28 +0900

    test を削除

--- joke_engine/joke/tests.py
+++ joke_engine/joke/tests.py
@@ -2,31 +2,31 @@ from django.test import TestCase
 
 # Create your tests here.
 
-import requests
-import json
-
-
-url = "http://localhost:8000/joke"
-
-headers = {'Content-type': 'application/json'}
-joke = '布団が吹っ飛んだ'
-
-
-# ダジャレ判定API
-res = requests.get(url + '/judge?joke=%s' % joke)
-res_json = json.loads(res.text)
-print('''
-ダジャレ判定
-    - ダジャレ：{}
-    - レスポンス：{}
-'''.format(joke, res_json))
-
-
-# ダジャレ評価API
-res = requests.get(url + '/evaluate?joke=%s' % joke)
-res_json = json.loads(res.text)
-print('''
-ダジャレ評価
-    - ダジャレ：{}
-    - レスポンス：{}
-'''.format(joke, res_json))
+# import os
+# import requests
+# import json
+#
+# url = "http://localhost:8000/joke"
+#
+# headers = {'Content-type': 'application/json'}
+# joke = '布団が吹っ飛んだ'
+#
+#
+# # ダジャレ判定API
+# res = requests.get(url + '/judge?joke=%s' % joke)
+# res_json = json.loads(res.text)
+# print('''
+# ダジャレ判定
+#     - ダジャレ：{}
+#     - レスポンス：{}
+# '''.format(joke, res_json))
+#
+#
+# # ダジャレ評価API
+# res = requests.get(url + '/evaluate?joke=%s' % joke)
+# res_json = json.loads(res.text)
+# print('''
+# ダジャレ評価
+#     - ダジャレ：{}
+#     - レスポンス：{}
+# '''.format(joke, res_json))
